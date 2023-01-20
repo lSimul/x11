b4e578d8fef1aa3122b9ccd274a5403f6928bc59
lukas.simulik <lukas.simulik@dubax.cz>
2023-01-20 10:21:00 +0100

    Pull code out to the function
    It did not work before anyway (XSync missing?).

--- presskey.c
+++ presskey.c
@@ -2,11 +2,21 @@
 #include <X11/keysym.h>
 #include <X11/extensions/XTest.h>
 
+#include <unistd.h>
+
+void pressKey(Display *display, KeySym);
+
 int main()
 {
 	Display *display = XOpenDisplay(NULL);
-	unsigned int keycode = XKeysymToKeycode(display, XK_A);
-	XTestFakeKeyEvent(display, keycode, True, 0);
-	XTestFakeKeyEvent(display, keycode, False, 0);
+	pressKey(display, XK_A);
+}
+
+void pressKey(Display *display, KeySym key)
+{
+	unsigned int keycode = XKeysymToKeycode(display, key);
+	XTestFakeKeyEvent(display, keycode, True, CurrentTime);
+	XTestFakeKeyEvent(display, keycode, False, CurrentTime);
 	XFlush(display);
+	XSync(display, False);
 }
