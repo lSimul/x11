c0a7a2652964b7f1497f559a5dbf94241540ed83
lukas.simulik <lukas.simulik@dubax.cz>
2023-01-23 10:56:04 +0100

    Be able to search for 2D image

--- image.c
+++ image.c
@@ -7,14 +7,22 @@
 int findImage(COORDS *result, BITMAP *needle, X_INSTANCE *instance)
 {
 	XImage *haystack = XGetImage(instance->display, instance->window, 0, 0, instance->attrs.width, instance->attrs.height, AllPlanes, ZPixmap);
+	if (needle->height == 1)
+	{
+		return findImageRow(result, needle, haystack);
+	}
+	return findComplexImage(result, needle, haystack, instance);
+}
 
+int findImageRow(COORDS *result, BITMAP *needle, XImage *haystack)
+{
 	int matched = 0;
 	for (int y = 0; y < haystack->height; y++)
 	{
 		for (int x = 0; x < haystack->width; x++)
 		{
 			unsigned long pixel = XGetPixel(haystack, x, y);
-			if (comparePixels(haystack, needle->data[matched], pixel))
+			if (comparePixel(haystack, needle->data[matched], pixel))
 			{
 				if (++matched == needle->size)
 				{
@@ -32,7 +40,47 @@ int findImage(COORDS *result, BITMAP *needle, X_INSTANCE *instance)
 	return 1;
 }
 
-static inline int comparePixels(XImage *img, PIXEL reference, unsigned long read)
+int findComplexImage(COORDS *result, BITMAP *needle, XImage *haystack, X_INSTANCE *instance)
+{
+	for (int y = 0; y < haystack->height; y++)
+	{
+		for (int x = 0; x < haystack->width; x++)
+		{
+			unsigned long pixel = XGetPixel(haystack, x, y);
+			if (comparePixel(haystack, needle->data[0], pixel))
+			{
+				if (x + needle->width > instance->attrs.width || y + needle->height > instance->attrs.height)
+				{
+					continue;
+				}
+
+				if (!findComplexImageInner(result, needle, haystack, x, y))
+				{
+					return 0;
+				}
+			}
+		}
+	}
+	return 1;
+}
+
+int findComplexImageInner(COORDS *result, BITMAP *needle, XImage *haystack, int offsetX, int offsetY)
+{
+	for (int y = 0; y < needle->height; y++)
+	{
+		for (int x = 0; x < needle->width; x++)
+		{
+			unsigned long pixel = XGetPixel(haystack, x + offsetX, y + offsetY);
+			if (!comparePixel(haystack, needle->data[x + y], pixel))
+			{
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
+static inline int comparePixel(XImage *img, PIXEL reference, unsigned long read)
 {
 	unsigned char blue = read & img->blue_mask;
 	unsigned char green = (read & img->green_mask) >> 8;
--- image.h
+++ image.h
@@ -1,26 +1,16 @@
 #include "common.h"
 #include "file.h"
 
-// Old comment from nonexisting file, losing relevance every new commit.
-//
-//
-// Main source of screen managing:
-//
-// https://stackoverflow.com/questions/8249669/how-do-take-a-screenshot-correctly-with-xlib
-//
-// It goes nicely from top-left to the bottom-right, column-wise.
-// Bytes how they are right now fit the top of the
-//
-// !!! Important note !!!
-// It does not start from position 0, 0 for me, but from 1, 1 for some reason.
-// No need to be sad or something like that, one pixel is really nothing, but for some
-// hardcore evaluation over multiple lines or fixed position it can cause some troubles.
-//
-// Read screen and based on the sequence of pixels
-// click on the position. (Just sequence, context is not checked.)
-
 /**
  * @brief
+ *
+ * Main source of screen managing:
+ *
+ * https://stackoverflow.com/questions/8249669/how-do-take-a-screenshot-correctly-with-xlib
+ *
+ * It does not start from position 0, 0 for me, but from 1, 1 for some reason.
+ * No need to be sad or something like that, one pixel is really nothing, but for some
+ * hardcore evaluation over multiple lines or fixed position it can cause some troubles.
  *
  * @param result
  * @param needle
@@ -33,9 +23,56 @@ int findImage(COORDS *result, BITMAP *needle, X_INSTANCE *instance);
 /**
  * @brief
  *
+ * @param result
+ * @param needle
+ * @param haystack
+ * @return int
+ */
+int findImageRow(COORDS *result, BITMAP *needle, XImage *haystack);
+
+/**
+ * @brief The most naive approach to looking for 2D image
+ *
+ * It does not use any searching optimizations, after first matching pixel
+ * we go full throttle into the findComplexImgeInner.
+ *
+ * Only one optimization is done, check for image size (needle outside of haystack
+ * is forbidden), which is more of a sanity check than an optimization.
+ *
+ * @todo Avoid more checks. When x is too big, x + 1 is also too big.
+ * @todo With complex knowledge of image, I should be able to skip bigger parts of the image
+ * (idea of the searching algorithm – movement can be faster, because needle cannot be _here_).
+ * @todo What about targeting smaller parts of the haystack?
+ *
+ * @param result
+ * @param needle
+ * @param haystack
+ * @param instance
+ * @return int
+ */
+int findComplexImage(COORDS *result, BITMAP *needle, XImage *haystack, X_INSTANCE *instance);
+
+/**
+ * @brief
+ *
+ * Uses the same idea as the findImageRow, but the tides are turned, reference is a haystack, not
+ * a needle.
+ *
+ * @param result
+ * @param needle
+ * @param haystack
+ * @param offsetX
+ * @param offsetY
+ * @return int
+ */
+int findComplexImageInner(COORDS *result, BITMAP *needle, XImage *haystack, int offsetX, int offsetY);
+
+/**
+ * @brief
+ *
  * @param img
  * @param reference
  * @param read
  * @return int
  */
-static inline int comparePixels(XImage *img, PIXEL reference, unsigned long read);
+static inline int comparePixel(XImage *img, PIXEL reference, unsigned long read);
